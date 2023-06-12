32f631b747702ee5eb7516ac2c12df50fbb86126
emmacodes <binma.emma@gmail.com>
2023-06-12 15:05:29 -0700

    attempted to display three colors in order

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Light from "./Component/Light"
+
 
 function App() {
+  const [light, setLight] = useState("red")
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLight("Yellow")
+      setTimeout(() => {
+        setLight("Green")
+      }, 2000)
+    }, 4000)
+
+  }, [light])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="w-screen h-screen bg-blue-100">
+      <Light light={light} />
+
+    </body>
   );
 }
 
---
+++ src/Component/Light.js
@@ -0,0 +1,6 @@
+import React from "react";
+export default function Light({ light }) {
+    return (
+       <div>{light}</div>
+    )
+}
